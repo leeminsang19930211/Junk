3cfdb480f8f22a5a19f6ad315d5aec760484ef0d
leeminsang19930211 <leeminsang19930211@gmail.com>
2018-11-17 14:00:24 +0900

    0

---
+++ SpaceShooter/Assets/02.Scripts/PlayerCtrl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCtrl :
+  MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+   void LateUpdate()
+    {
+
+    }
+}
